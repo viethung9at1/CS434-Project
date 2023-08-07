19a68efaa0d47325caa7503cd04cf86099d80f5f
viethung9at1 <viethung.9at1@gmail.com>
2023-08-07 20:51:47 +0700

    Add comment for chooseSrc

--- input/chooseSrc.py
+++ input/chooseSrc.py
@@ -1,7 +1,7 @@
 f= open('./input/rockyou.txt', 'r')
-i=100000
+i=100000 # NUmber of lines in input
 strToRead=list()
 for i in range(0, i):
    strToRead.append(f.readline())
-writeFile=open('./input/training.txt', 'w')
+writeFile=open('./input/training.txt', 'w') 
 writeFile.writelines(strToRead)
